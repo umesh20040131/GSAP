edf1cb07acdcda0ec258bccaed31a24189cb1ab0
umesh20040131 <uchaursiya6@gmail.com>
2025-10-18 14:08:04 +0530

    gsap basics

--- basics/script.js
+++ basics/script.js
@@ -10,25 +10,33 @@
 //    delay:1,
 //});
 
-gsap.to("#box1",{
-    x:1000,
-    duration:2,
-    delay:1,
-    rotation:360,
-});
+//gsap.to("#box1",{
+//    x:1000,
+//    duration:2,
+//    delay:1,
+//    rotation:360,
+//});
 
-gsap.to("#box2",{
-    x:1000,
-    duration:2,
-    delay:1,
-    backgroundColor:"yellow",
-});
+//gsap.to("#box2",{
+//    x:1000,
+//    duration:2,
+//    delay:1,
+//    backgroundColor:"yellow",
+//});
 
-gsap.to("#box3",{
-    x:1000,
+//gsap.to("#box3",{
+//    x:1000,
+//    duration:2,
+//    delay:1,
+//    backgroundColor:"purple",
+//    borderRadius:"50%",
+//    scale:0.5,
+//});
+
+gsap.to("h1",{
+    color:"red",
     duration:2,
-    delay:1,
-    backgroundColor:"purple",
-    borderRadius:"50%",
-    scale:0.5,
-});
+    delay:1
+})
+
+    
